9a874d74fdea3e132c89b262298f514fe1f8e0e1
shilna <mail2shilnamanohar@gmail.com>
2021-06-15 15:04:40 +0400

    test case 2

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -1,9 +1,66 @@
 import React from 'react'
+import styles from '../../styles/Footer.module.scss'
 
 export default function Footer() {
     return (
-        <div>
-            <h2>Footer</h2>
+        <div className={styles.footer}>
+            <div className={styles.top}>
+                <div className={styles.left}>
+                    <img src="icon.svg"></img>
+                </div>
+                <div className={styles.right}>
+                <ul>
+                    <li>
+                        <h6>Product</h6>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Tech Stack</a></li>
+                            <li><a href="#">super Merchants</a></li>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">Partners</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h6>About Us</h6>
+                        <ul>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">sectors</a></li>
+                            <li><a href="#">careers</a></li>
+                            <li><a href="#">contact</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h6>Doore</h6>
+                        <ul>
+                            <li><a href="#">Voluptate</a></li>
+                            <li><a href="#">Cilum</a></li>
+                            <li><a href="#">Pariatur</a></li>
+                            <li><a href="#">Nulla</a></li>
+                            <li><a href="#">Irure</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h6>Tertiary</h6>
+                        <ul>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Homterms & Conditions</a></li>
+                            <li><a href="#">Cookie Policy</a></li>
+                        </ul>
+                    </li>
+                </ul>
+             </div>
+             </div>
+             <div className={styles.bottom}>
+                <p className={styles.left}>© Kmmrce</p>
+                <p className={styles.left}>Site By Six</p>
+             </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,42 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import Footer from './components/footer'
 import Header from './components/header'
+import AOS from 'aos';
+import { useEffect } from "react";
+
+
 
 export default function Home() {
+
+  useEffect(() => {
+    AOS.init({
+      duration : 2000
+    });
+  }, []);
+  
   return (
     <div>
       <Header/> 
       <div className="platform">
-          
+          <div className="description" data-aos="zoom-in">
+              <h1><span>KMMRCE</span> is an enterprise level digital platform that provides a turnkey solution to online retailing.</h1>
+              <button className="km-btn">Request Demo &gt;</button>
+          </div>
+          <div className="img-block">
+              <div className="img-rotation"></div>
+              <img src="speaker.png"/>
+          </div>
+      </div>
+      <div className="sell-digital">
+          <img src="1.png" className="img-1" data-aos="flip-up"></img>
+          <img src="2.png" className="img-2" data-aos="flip-up"></img>
+          <img src="3.png" className="img-3" data-aos="flip-up"></img>
+          <img src="4.png" className="img-4" data-aos="flip-up"></img>
+          <img src="5.png" className="img-5" data-aos="flip-up"></img>
+          <img src="6.png" className="img-6" data-aos="flip-up"></img>
+          <span data-aos="slide-up">Platform</span>
+          <h2 data-aos="slide-up">Sell digital or physical inventory, with configurable or simple attributes, in a way that suits your business.</h2>
+          <button className="km-white-btn" data-aos="slide-up">Explore the Platform ›</button>
       </div>
       <Footer/>
     </div>
